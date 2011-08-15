d26aaec78a107278b4cd3142650527be9a9dd220
Alan Rothkopf <arothkopf@usa.com>
2011-08-15 18:35:22 +0200

    Separate Main classes for different installations

---
+++ src/org/scramble/ui/SwingAppWinEN.java
@@ -0,0 +1,22 @@
+/**
+ * Scramble - Copyright (c) 2011 Alan Rothkopf 
+ * Source code released under GPLv3 ( http://www.gnu.org/licenses/gpl-3.0.html )
+ */
+package org.scramble.ui;
+
+/**
+ * Main class for English-language Windows deployment
+ * @author alan
+ *
+ */
+public class SwingAppWinEN {
+	private static final String[] ARGS = {"en_US","win"};
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SwingApp.main(ARGS);
+	}
+
+}
---
+++ src/org/scramble/ui/SwingAppWinFR.java
@@ -0,0 +1,22 @@
+/**
+ * Scramble - Copyright (c) 2011 Alan Rothkopf 
+ * Source code released under GPLv3 ( http://www.gnu.org/licenses/gpl-3.0.html )
+ */
+package org.scramble.ui;
+
+/**
+ * Main class for French-language Windows deployment
+ * @author alan
+ *
+ */
+public class SwingAppWinFR {
+	private static final String[] ARGS = {"fr_FR","win"};
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SwingApp.main(ARGS);
+	}
+
+}
